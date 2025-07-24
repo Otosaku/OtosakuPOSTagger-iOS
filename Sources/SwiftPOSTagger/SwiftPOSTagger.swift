b88c78721367c0418e51ea95770147418e6ac612
otosaku-ai <otosaku.dsp@gmail.com>
2025-07-24 13:17:30 +0300

    Add SwiftPOSTagger class with model loading and tokenizer integration

--- Sources/SwiftPOSTagger/SwiftPOSTagger.swift
+++ Sources/SwiftPOSTagger/SwiftPOSTagger.swift
@@ -1,2 +1,20 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+import CoreML
+
+public class SwiftPOSTagger {
+    private let model: MLModel
+    private let tokenizer: BertTokenizer
+    
+    public init(modelDirectoryURL: URL, computeUnits: MLComputeUnits = .all) throws {
+        let modelURL = modelDirectoryURL.appendingPathComponent("Model.mlmodelc")
+        let vocabURL = modelDirectoryURL.appendingPathComponent("vocab.txt")
+        
+        let configuration = MLModelConfiguration()
+        configuration.computeUnits = computeUnits
+        self.model = try MLModel(contentsOf: modelURL, configuration: configuration)
+        self.tokenizer = BertTokenizer(vocab: vocabURL)
+    }
+}
